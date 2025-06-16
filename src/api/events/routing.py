2d636fe3612c8d35c003c8e92b7a05a3a8dc6078
Igor <xxxigorbiesiada1@gmail.com>
2025-06-16 20:21:56 +0200

    schemas, routing and send data

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .schemas import EventSchema, EventListSchema
+from .schemas import EventSchema, EventListSchema, EventCreateSchema, EventUpdateSchema
 
 router = APIRouter()
 
@@ -17,17 +17,17 @@ def read_events() -> EventListSchema:
 
 #send data
 @router.post("/")
-def send_events() -> EventListSchema:
-    return {
-       "result": [
-           {"id":1},
-           {"id":2},
-           {"id":3}
-       ],
-        "count": 3
-    }
+def create_events(payload:EventCreateSchema) -> EventSchema:
+    print(payload.page)
+    data = payload.model_dump()     #payload -> dict
+    return {"id": 123, **data}
 
 
 @router.get("/{event_id}")
 def get_event(event_id:int) -> EventSchema:
     return {"id": event_id}
+
+@router.put("/{event_id}")
+def update_event(event_id:int, payload:EventUpdateSchema) -> EventSchema:
+    data = payload.model_dump()
+    return {"id": event_id, **data}
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,8 +1,17 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel
+
+class EventCreateSchema(BaseModel):
+    page: str
+    description: Optional[str] = ''
+
+class EventUpdateSchema(BaseModel):
+    description: str
 
 class EventSchema(BaseModel):
     id: int
+    page: Optional[str] = ''
+    description: Optional[str] = ''
 
 class EventListSchema(BaseModel):
     result: List[EventSchema]
