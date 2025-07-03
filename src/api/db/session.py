9e40a6efe5dd72c45f48c63a1325cf6abaa6003f
Igor <xxxigorbiesiada1@gmail.com>
2025-07-03 18:42:53 +0200

    models, session and  config

--- src/api/db/session.py
+++ src/api/db/session.py
@@ -1,16 +1,19 @@
 import sqlmodel
-from .config import DATABASE_URL
+from .config import DATABASE_URL, DB_TIMEZONE
 from sqlmodel import SQLModel, Session
+import timescaledb
 
 if DATABASE_URL == "":
     raise NotImplementedError("DATABASE_URL need to be set")
 
-engine = sqlmodel.create_engine(DATABASE_URL)
+engine = timescaledb.create_engine(DATABASE_URL, timezone=DB_TIMEZONE)
 
 
 def init_db():
     print('creating db')
     SQLModel.metadata.create_all(engine)
+    #print('creating hypertables')
+    #timescaledb.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
--- src/api/events/models.py
+++ src/api/events/models.py
@@ -2,9 +2,12 @@ from datetime import datetime, timezone
 from typing import List, Optional
 from sqlmodel import SQLModel, Field
 import sqlmodel
+from timescaledb import TimescaleModel
+from timescaledb.utils import get_utc_now
 
-def get_utc_now():
-    return datetime.now(timezone.utc).replace(tzinfo=timezone.utc)
+# using get_utc_now function from timescaledb.utils
+#def get_utc_now():
+#    return datetime.now(timezone.utc).replace(tzinfo=timezone.utc)
 
 
 class EventCreateSchema(SQLModel):
@@ -14,20 +17,24 @@ class EventCreateSchema(SQLModel):
 class EventUpdateSchema(SQLModel):
     description: str
 
-class EventModel(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    page: Optional[str] = ''
+#tracking page visits at any given time
+
+class EventModel(TimescaleModel, table=True):
+    #id: int | None = Field(default=None, primary_key=True)
+    page: str = Field(index=True) #/about or /contact page 
     description: Optional[str] = ''
-    created_at: datetime = Field(
-        default_factory=get_utc_now,
-        sa_type=sqlmodel.DateTime(timezone=True),
-        nullable=False
-)
+    #created_at: datetime = Field(
+    #    default_factory=get_utc_now,
+    #    sa_type=sqlmodel.DateTime(timezone=True),
+    #    nullable=False
+    #)
     updated_at: datetime = Field(
         default_factory=get_utc_now,
         sa_type=sqlmodel.DateTime(timezone=True),
         nullable=False
 )
+    __chunk_time_interval__= "INTERVAL 1 day"
+    __drop_after__ = "INTERVAL 3 months"
 
 class EventListSchema(SQLModel):
     result: List[EventModel]
